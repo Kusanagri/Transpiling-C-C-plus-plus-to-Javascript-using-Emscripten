fe375e4ed08abaeb951016a4bb40de7fdaeb223a
Shivendu Singh <36742363+Kusanagri@users.noreply.github.com>
2018-05-05 04:30:32 +0530

    Create multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,17 @@
+#include<emscripten.h>
+
+
+extern "C"{ int multiply_in_js(float, float); }
+
+extern "C" {
+int EMSCRIPTEN_KEEPALIVE multiply(float x, float y)
+{
+ return multiply_in_js(x, y);
+ 
+}
+}
+
+int main()
+{
+ return 0;
+}
